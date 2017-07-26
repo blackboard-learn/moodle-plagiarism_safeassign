bf3da386180c525e06014ec33421411a2697217c
Guillermo Alvarez <guillermo.alvarez@blackboard.com>
2017-07-26 17:17:31 -0500

    INT-10770: Create scaffold for SafeAssign

---
+++ plagiarism_form.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   safeassign
+ * @copyright Copyright (c) 2017 Blackboard Inc.
+ */
+
+require_once($CFG->dirroot.'/lib/formslib.php');
+
+class plagiarism_setup_form extends moodleform {
+
+/// Define the form
+    function definition () {
+        global $CFG;
+        $mform =& $this->_form;
+
+        $mform->addElement('html', get_string('safeassignexplain', 'plagiarism_safeassign'));
+        $mform->addElement('checkbox', 'new_use', get_string('usesafeassign', 'plagiarism_safeassign'));
+
+        $mform->addElement('textarea', 'new_student_disclosure', get_string('studentdisclosure','plagiarism_safeassign'),'wrap="virtual" rows="6" cols="50"');
+        $mform->addHelpButton('new_student_disclosure', 'studentdisclosure', 'plagiarism_safeassign');
+        $mform->setDefault('new_student_disclosure', get_string('studentdisclosuredefault','plagiarism_safeassign'));
+
+        $this->add_action_buttons(true);
+    }
+}
+
