c122c5b9b47b79fb6aa4fabaebde63ca215a30e9
Juan Martinez <juan.martinez@blackboard.com>
2017-09-26 00:06:20 +0000

    INT-12393: Added the SafeAssign tag to .kow-ci.yml and fixed safeassign submission behat tests.

---
+++ tests/behat/behat_plagiarism_safeassign.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Steps definitions for behat.
+ *
+ * @package   plagiarism_safeassign
+ * @category  test
+ * @copyright Copyright (c) 2017 Blackboard Inc.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../../lib/behat/behat_base.php');
+
+/**
+ * Class behat_plagiarism_safeassign
+ *
+ * @package   plagiarism_safeassign
+ * @category  test
+ * @copyright Copyright (c) 2017 Blackboard Inc.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class behat_plagiarism_safeassign extends behat_base {
+
+    /**
+     * @Given /^I am on the course "(?P<shortname_string>(?:[^"]|\\")*)"$/
+     * @param string $shortname
+     */
+    public function i_am_on_the_course($shortname) {
+        global $DB;
+        $courseid = $DB->get_field('course', 'id', ['shortname' => $shortname]);
+        $this->getSession()->visit($this->locate_path('/course/view.php?id='.$courseid));
+    }
+
+}
