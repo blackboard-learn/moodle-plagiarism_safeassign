4744f19885c36e441a38e033a1c1389b44a5d10d
Jonathan Garcia <jonathan.garcia@blackboard.com>
2019-11-26 15:31:23 -0500

    INT-15220: Version and required version bump

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2019092401;
-$plugin->release = '3.6.3';
-$plugin->requires = 2018120300;
+$plugin->release = '3.7.3';
+$plugin->requires = 2019052000;
 $plugin->component = 'plagiarism_safeassign';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0;
