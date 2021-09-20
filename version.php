41ac10bb0119b4cd61ab6c17cd08579dc70399b1
Fabian Batioja <fabian.batioja@openlms.net>
2021-09-20 16:03:46 +0000

    INT-17437: Bump versions for 3.11: After merge

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2021060101;
-$plugin->release = '3.10.3';
-$plugin->requires = 2020110900;
+$plugin->release = '3.11.2';
+$plugin->requires = 2021051700;
 $plugin->component = 'plagiarism_safeassign';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0;
