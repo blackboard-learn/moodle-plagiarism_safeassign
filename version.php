bdfc876ef1e32fc98c359369b1df6af69797acf5
Rafael Monterroza <Rafael.Monterroza@openlms.net>
2022-10-12 12:54:16 -0500

    INT-18217: Bump versions for 3.11 MP5

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022072900;
+$plugin->version = 2022101200;
 $plugin->release = '3.11.6';
 $plugin->requires = 2021051700;
 $plugin->component = 'plagiarism_safeassign';
