9fc981787ac6d8ff12742fa8c34acc8bf93a8b36
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2022-12-21 23:09:52 +0000

    INT-18550: 4.0 Version bump

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022101200;
-$plugin->release = '3.11.6';
-$plugin->requires = 2021051700;
+$plugin->version = 2022122100;
+$plugin->release = '4.0.5';
+$plugin->requires = 2022041900;
 $plugin->component = 'plagiarism_safeassign';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron      = 0;
